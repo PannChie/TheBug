734511f2e291598b1f6c984a3d5068439144badd
PannChie <bridapann@gmail.com>
2017-12-09 18:34:09 -0300

    Adcionando terreno e texturas: entrada

---
+++ Assets/Scripts/SceneManeger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManeger : MonoBehaviour {
+
+	public string nomeDaCenaAtual;
+
+	// Use this for initialization
+	void Start () {
+
+		nomeDaCenaAtual = SceneManager.GetActiveScene ().name;
+		SceneManager.LoadScene ("InfernoTokastu1");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
